6f4ad4b784c03a6a9187705a0bbf3f6beaf6cc23
Felix <felixadrianlucaciu@gmail.com>
2017-02-22 19:22:12 -0500

    Hello world test

---
+++ COMP371_Project/Debug/src/COMP371_Project.d
@@ -0,0 +1,1 @@
+src/COMP371_Project.d: ../src/COMP371_Project.cpp
---
+++ COMP371_Project/src/COMP371_Project.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : COMP371_Project.cpp
+// Author      : Felix Lucaciu
+// Version     :1.0
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
