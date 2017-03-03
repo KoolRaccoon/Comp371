2be7d65d743d31a10b19e683499eff98ee359d2d
dsecca <davsecc1995@gmail.com>
2017-03-03 15:11:32 -0500

    David's first commit

--- COMP371_Project/src/COMP371_Project.cpp
+++ COMP371_Project/src/COMP371_Project.cpp
@@ -12,5 +12,6 @@ using namespace std;
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	cout << "test from Ahmad" << endl;
+	cout << "David's first push" << endl;
 	return 0;
 }
